e46c08194d7db4d2fdde23d3a06f3778eda6b87d
Felipe Sferra <felipesferra@gmail.com>
2023-08-24 16:03:47 -0300

    Montagem dos menus

--- projeto_lingomax.cpp
+++ projeto_lingomax.cpp
@@ -53,11 +53,39 @@ struct Users
 // Menus
 
 void menuUsers();
+void menuLanguages();
 void menuExercises();
 void menuLessons();
-void menuLanguages();
 
 // Protótipos de função
+
+void includeUser();
+void deleteUser();
+void rearrangeUser();
+void exhaustiveUser();
+bool searchUser();
+void indexUser();
+
+void includeLanguage();
+void deleteLanguage();
+void rearrangeLanguage();
+void exhaustiveLanguage();
+bool searchLanguage();
+void indexLanguage();
+
+void includeExercise();
+void deleteExercise();
+void rearrangeExercise();
+void exhaustiveExercise();
+bool searchExercise();
+void indexExercise();
+
+void includeLesson();
+void deleteLesson();
+void rearrangeLesson();
+void exhaustiveLesson();
+bool searchLesson();
+void indexLesson();
 
 // Programa principal
 
@@ -109,3 +137,46 @@ int main()
 
     SetConsoleOutputCP(CPAGE_DEFAULT);
 }
+
+void menuUsers()
+{
+    int op = 999;
+
+    while (op != 0)
+    {
+        system("cls");
+        cout << "\t\tMenu Usuários - LingoMax\n\n";
+        cout << "[01] - Incluir Usuário\n";
+        cout << "[02] - Excluir Usuário\n";
+        cout << "[03] - Reorganizar Tabela\n";
+        cout << "[04] - Leitura Exaustiva\n";
+        cout << "[00] - Voltar para o menu principal\n";
+        cout << "Digite a opção desejada: ";
+        cin >> op;
+
+        switch (op)
+        {
+        case 1:
+            includeUser();
+            break;
+        case 2:
+            deleteUser();
+            break;
+        case 3:
+            rearrangeUser();
+            break;
+        case 4:
+            exhaustiveUser();
+            break;
+        case 0:
+            break;
+        default:
+            cout << "Opção inválida!\n";
+            system("pause");
+        }
+    }
+}
+
+void menuLanguages() {}
+void menuExercises() {}
+void menuLessons() {}
